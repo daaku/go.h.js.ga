4065a13fc490323a671b4315c2af5283d4af4a49
Naitik Shah <n@daaku.org>
2012-05-07 13:29:19 -0700

    initial commit

---
+++ ga.go
@@ -0,0 +1,28 @@
+// Package ga provides a go.h.js.loader compatible async loading for
+// Google Analytics.
+package ga
+
+import (
+	"fmt"
+	"log"
+)
+
+// Loadable for a Page Track event using Google Analytics.
+type Track struct {
+	ID string
+}
+
+func (g *Track) URLs() []string {
+	return []string{"https://ssl.google-analytics.com/ga.js"}
+}
+
+func (g *Track) Script() string {
+	if g.ID == "" {
+		log.Fatal("GoogleAnalyics requires an ID.")
+	}
+	return fmt.Sprintf(
+		`try{`+
+			`var pageTracker=_gat._getTracker("%s");`+
+			`pageTracker._trackPageview();`+
+			`}catch(e){}`, g.ID)
+}
