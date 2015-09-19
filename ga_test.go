14a7e5df55ccabdfa57bb1e913e17ae14ff6c6ef
Naitik Shah <n@daaku.org>
2015-09-19 10:37:53 -0700

    use more modern GA JS

--- ga_test.go
+++ ga_test.go
@@ -19,7 +19,11 @@ func TestTrack(t *testing.T) {
 	s, err := h.Render(context.Background(), &Track{Account: "a42"})
 	ensure.Nil(t, err)
 	ensure.DeepEqual(t, s,
-		`<script>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'a42']);`+
-			`_gaq.push(['_trackPageview']);</script><script src="`+
-			`https://ssl.google-analytics.com/ga.js" async></script>`)
+		`<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;`+
+			`i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},`+
+			`i[r].l=1*new Date();a=s.createElement(o),`+
+			`m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;`+
+			`m.parentNode.insertBefore(a,m)})(window,document,'script',`+
+			`'//www.google-analytics.com/analytics.js','ga');`+
+			`ga('create','a42','auto');ga('send','pageview');</script>`)
 }
